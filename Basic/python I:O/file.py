9a9b14160f30bc570e9350ccd3cdd02ec33d34a7
Tanvir Mahmud <tanvirmahmud@Tanvirs-MacBook-Air.local>
2023-03-27 01:15:36 +0600

    file handling in python

---
+++ Basic/python I:O/file.py
@@ -0,0 +1,7 @@
+import os
+
+# to read exiting file on drive
+f = open( "/Users/tanvirmahmud/Documents/Programming/Python/Python-basic-/Basic/python I:O/text.txt", "r")
+print(f.readline())
+
+# to create and write
